9271c085892dbb5dd40f90db76cca3f673fd5a52
Tiago <tiago.linhares@live.com>
2022-01-24 13:37:21 -0300

    paginas dinamicas com jsp

--- src/main/java/br/com/gerenciador/servelet/ListaEmpresasServlet.java
+++ src/main/java/br/com/gerenciador/servelet/ListaEmpresasServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,25 +24,13 @@ public class ListaEmpresasServlet extends HttpServlet {
 		
 		Banco banco = new Banco();
 		List<Empresa> lista = banco.getEmpresas();
-		PrintWriter out = response.getWriter();
 		
-		out.println("<html><body>");
-        out.println("<ul>");
-        for (Empresa empresa : lista) {
-			out.println("<li>" + empresa.getNome() + "</li>");
-		}
-        
-        out.println("</ul>");
-        out.println("</body></html>");
+		request.setAttribute("empresas", lista);
 		
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas.jsp");
+		rd.forward(request, response);
+		
+		
 	}
 
 }
